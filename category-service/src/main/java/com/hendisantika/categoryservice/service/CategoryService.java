8bc66fb185882b69078def2e12048fd768598ad3
hendisantika <hendisantika@yahoo.co.id>
2023-11-04 08:45:31 +0700

    #7 - Add Category Service in Category Service ✏️📗🐳🐧

---
+++ category-service/src/main/java/com/hendisantika/categoryservice/service/CategoryService.java
@@ -0,0 +1,24 @@
+package com.hendisantika.categoryservice.service;
+
+import com.hendisantika.categoryservice.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-gateway-k8s
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/4/23
+ * Time: 08:45
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+}

ac160a9489c87f3de40d006baf2e78bfd624045c
hendisantika <hendisantika@yahoo.co.id>
2023-11-04 08:44:06 +0700

    #5 - Add Category Entity in Category Service ✏️📗🐳🐧

---
+++ category-service/src/main/java/com/hendisantika/categoryservice/entity/Category.java
@@ -0,0 +1,44 @@
+package com.hendisantika.categoryservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-gateway-k8s
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/4/23
+ * Time: 08:43
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "deleted", nullable = false)
+    private Integer deleted;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}

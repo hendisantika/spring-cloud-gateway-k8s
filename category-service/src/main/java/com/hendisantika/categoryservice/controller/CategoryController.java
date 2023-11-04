28b5b48f4f4b29c5f5d9ced9bea2946b51385afb
hendisantika <hendisantika@yahoo.co.id>
2023-11-04 08:50:48 +0700

    #17 - Add save in CategoryController in Category Service ✏️📗🐳🐧

--- category-service/src/main/java/com/hendisantika/categoryservice/controller/CategoryController.java
+++ category-service/src/main/java/com/hendisantika/categoryservice/controller/CategoryController.java
@@ -1,7 +1,12 @@
 package com.hendisantika.categoryservice.controller;
 
+import com.hendisantika.categoryservice.entity.Category;
 import com.hendisantika.categoryservice.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @PostMapping(value = "${controller.api.create}")
+    public ResponseEntity<Category> save(@RequestBody Category category) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(categoryService.save(category));
+    }
 }

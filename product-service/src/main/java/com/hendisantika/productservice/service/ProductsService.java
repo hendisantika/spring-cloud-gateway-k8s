722a02f1c47a1d45f040730b337c7cb5ae075d28
hendisantika <hendisantika@yahoo.co.id>
2023-11-04 09:11:16 +0700

    #28 - Enable Discovery Client & Feign in Product Service ✏️📗🐳🐧

---
+++ product-service/src/main/java/com/hendisantika/productservice/service/ProductsService.java
@@ -0,0 +1,14 @@
+package com.hendisantika.productservice.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-gateway-k8s
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/4/23
+ * Time: 09:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProductsService {
+}

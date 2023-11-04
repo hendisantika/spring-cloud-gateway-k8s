c3fcf363c5cef5b64febf59ffc1ab7e4aa0095b0
hendisantika <hendisantika@yahoo.co.id>
2023-11-04 08:48:57 +0700

    #14 - Add ConstantValue in Category Service ✏️📗🐳🐧

---
+++ category-service/src/main/java/com/hendisantika/categoryservice/model/ConstantValue.java
@@ -0,0 +1,16 @@
+package com.hendisantika.categoryservice.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-gateway-k8s
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/4/23
+ * Time: 08:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConstantValue {
+    public static final String NOT_FOUND_MESSAGE = "data is not found";
+    public static final String NOT_FOUND_STATUS = "404";
+}

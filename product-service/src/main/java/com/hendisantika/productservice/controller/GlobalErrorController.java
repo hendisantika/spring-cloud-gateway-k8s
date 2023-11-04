ac04cde2e96a3c250b843d6344479153e8ef5d86
hendisantika <hendisantika@yahoo.co.id>
2023-11-04 09:23:39 +0700

    #47 - Add handleNotfoundException in GlobalErrorController in Product Service ✏️📗🐳🐧

--- product-service/src/main/java/com/hendisantika/productservice/controller/GlobalErrorController.java
+++ product-service/src/main/java/com/hendisantika/productservice/controller/GlobalErrorController.java
@@ -1,8 +1,16 @@
 package com.hendisantika.productservice.controller;
 
+import com.hendisantika.productservice.exception.MessageNotfoundException;
+import com.hendisantika.productservice.model.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static com.hendisantika.productservice.model.ConstantValue.NOT_FOUND_MESSAGE;
+import static com.hendisantika.productservice.model.ConstantValue.NOT_FOUND_STATUS;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,5 +25,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @Slf4j
 @RestControllerAdvice
 public class GlobalErrorController extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(MessageNotfoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotfoundException(MessageNotfoundException e) {
+        log.info("handleNotfoundException.error = {} ", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponse.builder()
+                        .errorMessage(NOT_FOUND_MESSAGE)
+                        .errorStatus(NOT_FOUND_STATUS)
+                        .build());
+    }
 
 }
